126c5d19b7d0b378d3ed8acf629f5824fc7756de
Deell <deellmurad@gmail.com>
2016-01-13 11:29:20 +0300

    class

---
+++ blabla/FACULTIES.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blabla
+{
+    class FACULTIES
+    {
+        public int IDfucalty { get; set; }
+        public string NameFucalty { get; set; }
+        public string quantity { get; set; }
+    }
+}
---
+++ blabla/STUDENTS.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blabla
+{
+    class STUDENTS
+    {
+        public int StudentsName { get; set; }
+        public string course { get; set; }
+        public string rating { get; set; }
+
+       
+
+    }
+}
